ef11fab89e97826b0fc73d705ebb7559279634d2
Axel Shira Sapata Negara <axelshira88@gmail.com>
2022-12-12 02:04:02 +0700

    Update donation-page.dart

--- tubes_uid/lib/donation-page.dart
+++ tubes_uid/lib/donation-page.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -11,6 +13,41 @@ class DonationPage extends StatefulWidget {
 class _DonationPageState extends State<DonationPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        // Image.asset(
+        //   "assets/doodle.png",
+        // height: MediaQuery.of(context).size.height,
+        // width: MediaQuery.of(context).size.width,
+        //   fit: BoxFit.cover,
+        // ),
+        Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: Color(0xFFFFEFC7),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(21, 45, 0, 6),
+                height: 96,
+                width: MediaQuery.of(context).size.width,
+                color: Color(0xFFBB9457),
+                child: Row(
+                  children: [
+                    Text(
+                      "Donation",
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFFF1F1F1)),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
