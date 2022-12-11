dbb929c5c46616dbc007cb1cae5606bf5d1f3319
Axel Shira Sapata Negara <axelshira88@gmail.com>
2022-12-11 23:06:41 +0700

    Create adoption-page.dart

---
+++ tubes_uid/lib/adoption-page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AdoptionPage extends StatefulWidget {
+  const AdoptionPage({super.key});
+
+  @override
+  State<AdoptionPage> createState() => _AdoptionPageState();
+}
+
+class _AdoptionPageState extends State<AdoptionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
