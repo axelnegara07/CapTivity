a8979c9fa424210005a7c3fb780849b98b0a8137
Axel Shira Sapata Negara <axelshira88@gmail.com>
2022-12-11 23:06:47 +0700

    Create shop-page.dart

---
+++ tubes_uid/lib/shop-page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ShopPage extends StatefulWidget {
+  const ShopPage({super.key});
+
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
