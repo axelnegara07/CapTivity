59094c4eb27c394135883005cfdc3b140e1a6122
Axel Shira Sapata Negara <axelshira88@gmail.com>
2022-12-11 23:06:45 +0700

    Create donation-page.dart

---
+++ tubes_uid/lib/donation-page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DonationPage extends StatefulWidget {
+  const DonationPage({super.key});
+
+  @override
+  State<DonationPage> createState() => _DonationPageState();
+}
+
+class _DonationPageState extends State<DonationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
