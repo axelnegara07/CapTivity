0fadb427e10eb70da1f5659b7ff424296fc0d494
Axel Shira Sapata Negara <axelshira88@gmail.com>
2022-12-19 03:12:36 +0700

    Update shop-page.dart

--- tubes_uid/lib/shop-page.dart
+++ tubes_uid/lib/shop-page.dart
@@ -11,36 +11,34 @@ class ShopPage extends StatefulWidget {
 }
 
 class _ShopPageState extends State<ShopPage> {
+  TextEditingController search = TextEditingController();
   List<String> categoryPhotos = [
-    "assets/category/dog.png",
-    "assets/category/cat.png",
-    "assets/category/rabbit.png",
-    "assets/category/hamster.png",
-    "assets/category/bird.png"
+    "assets/shop/popular/satu.png",
+    "assets/shop/popular/dua.png",
+    "assets/shop/popular/tiga.png",
+    "assets/shop/popular/empat.png",
+    "assets/shop/popular/lima.png"
   ];
 
-  List<String> informationPhotos = [
-    "assets/information/golden.png",
-    "assets/information/belgian.png",
-    "assets/information/chihuahua.png",
-    "assets/information/poodle.png",
-    "assets/information/cat.png"
+  List<String> productPhotos = [
+    "assets/shop/all/satu.png",
+    "assets/shop/all/dua.png",
+    "assets/shop/all/tiga.png",
+    "assets/shop/all/empat.png",
   ];
 
-  List<String> informationTittle = [
-    "Golden Retriever",
-    "Belgian Malinois",
-    "Chihuahua",
-    "Poodle",
-    "Common Cat"
+  List<String> productTittle = [
+    "Labrador T-shirt",
+    "Black Dog T-shirt",
+    "Aplique Zebra",
+    "Zebra Party Bag",
   ];
 
-  List<String> informationDesc = [
-    "The Golden Retriever is a sturdy, muscular dog of medium size, famous for the dense, lustrous...",
-    "Belgian Malinois are squarely built, proud, and alert herders standing 22 to 26 inches. Strong and well...",
-    "The Chihuahua is a balanced, graceful dog of terrier-like demeanor, weighing no more...",
-    "Poodles are extremely intelligent and are easily trained. They are agile and graceful as well as...",
-    "The cat is a domestic species of small carnivorous mammal."
+  List<String> productPrice = [
+    "Rp 70.000",
+    "Rp 50.000",
+    "Rp 15.000",
+    "Rp 20.000",
   ];
   @override
   Widget build(BuildContext context) {
@@ -75,165 +73,184 @@ class _ShopPageState extends State<ShopPage> {
                   ],
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(24, 10, 24, 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
+              Container(
+                padding: EdgeInsets.fromLTRB(0, 13, 0, 10),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 105,
+                          ),
+                          Text(
+                            "Our Popular Items",
+                            style: TextStyle(
+                              fontSize: 24,
+                              color: Color(0xFF575757),
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 55,
+                          ),
+                          Icon(
+                            Icons.shopping_cart,
+                            size: 30,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 124,
+                      width: 124,
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemCount: categoryPhotos.length,
+                          itemBuilder: (context, i) {
+                            return Padding(
+                              padding: EdgeInsets.only(right: 24),
+                              child: Container(
+                                height: 124,
+                                width: 124,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  image: DecorationImage(
+                                    image: AssetImage(categoryPhotos[i]),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            );
+                          }),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 33,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 24,
+                  ),
+                  Container(
+                    height: 36,
+                    width: MediaQuery.of(context).size.width - 48,
+                    child: Row(
                       children: [
                         Text(
-                          "Our Popular Items",
-                          style:
-                              TextStyle(fontSize: 24, color: Color(0xFF575757)),
+                          "Buy Now",
+                          style: TextStyle(
+                              fontSize: 24,
+                              color: Color(0xFF575757),
+                              fontWeight: FontWeight.w500),
                         ),
                         SizedBox(
-                          width: 55,
+                          width: 79,
                         ),
-                        Icon(Icons.shopping_cart),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Container(
-                            height: 123,
-                            width: 123,
-                            child: ListView.builder(
-                                shrinkWrap: true,
-                                scrollDirection: Axis.horizontal,
-                                itemCount: categoryPhotos.length,
-                                itemBuilder: (context, i) {
-                                  return Padding(
-                                    padding: EdgeInsets.only(right: 24),
-                                    child: Container(
-                                      height: 160,
-                                      width: 125,
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10),
-                                        image: DecorationImage(
-                                          image: AssetImage(categoryPhotos[i]),
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
-                                    ),
-                                  );
-                                }),
+                        Flexible(
+                          flex: 1,
+                          child: TextFormField(
+                            controller: search,
+                            style: TextStyle(fontSize: 12),
+                            decoration: InputDecoration(
+                                isDense: true,
+                                hintText: 'Search...',
+                                hintStyle: TextStyle(
+                                    color: Color(0xFF5C4E4E).withOpacity(0.45)),
+                                prefixIcon: Icon(
+                                  Icons.search,
+                                  color: Color(0xFF5C4E4E),
+                                  size: 24,
+                                ),
+                                filled: true,
+                                fillColor: Color(0XFFF2F2F2),
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: BorderSide(
+                                        width: 0, style: BorderStyle.none))),
                           ),
-                        )
+                        ),
                       ],
                     ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Center(
-                      child: Text(
-                        "Information",
-                        style:
-                            TextStyle(fontSize: 24, color: Color(0xFF575757)),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(33, 0, 33, 0),
+                child: GridView.count(
+                  shrinkWrap: true,
+                  childAspectRatio: 0.8,
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 29,
+                  crossAxisSpacing: 40,
+                  children: List.generate(productPhotos.length, (i) {
+                    return InkWell(
+                      onTap: () {
+                        //berbeda dengan genre, language hanya bisa terpilih 1
+                        //konsep : ubah semua isSelected languages jadi false dan isi language yang terpilih jadi true
+                        // for(var j = 0; j < languages.length; j++){
+                        // languages[j].isSelected = false;
+                        // }
+                        // languages[i].isSelected = true; //ini diassign true dan tidak ada ! karena dicontoh flutix jika yg udah keselect dipencet maka tidak akan hilang
+                        // setState(() {});
+                      },
+                      child: Container(
+                        // padding: EdgeInsets.fromLTRB(42, 27, 42, 21),
+                        decoration: BoxDecoration(
+                          // image: DecorationImage(
+                          //     image: AssetImage(productPhotos[i])),
+                          color: Colors.transparent,
+                          // color: languages[i].isSelected ? Color(0XFFFBD45F) : Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          // border: Border.all(color: languages[i].isSelected ? Color(0XFFFBD45F) :  Color(0XFFE7E7E7)),
+                        ),
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 130,
+                              width: 130,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(productPhotos[i]),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              productTittle[i],
+                              style: TextStyle(
+                                  color: Color(0XFF000000),
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            Text(
+                              productPrice[i],
+                              style: TextStyle(
+                                color: Color(0XFF000000),
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          // error
-                          // height: 480,
-                          height: 423,
-                          width: 380,
-                          child: Expanded(
-                            child: Container(
-                                height: 122,
-                                width: 380,
-                                child: MediaQuery.removePadding(
-                                  context: context,
-                                  removeTop: true,
-                                  removeBottom: true,
-                                  child: ListView.builder(
-                                      shrinkWrap: true,
-                                      scrollDirection: Axis.vertical,
-                                      itemCount: informationPhotos.length,
-                                      itemBuilder: (context, i) {
-                                        return Padding(
-                                          padding: EdgeInsets.only(bottom: 15),
-                                          child: Container(
-                                              height: 122,
-                                              width: 380,
-                                              decoration: BoxDecoration(
-                                                color: Color(0xFFBB9457),
-                                                borderRadius:
-                                                    BorderRadius.circular(10),
-                                              ),
-                                              child: Padding(
-                                                // right 16 padding
-                                                padding: EdgeInsets.fromLTRB(
-                                                    17, 14, 0, 14),
-                                                child: Row(
-                                                  children: [
-                                                    Container(
-                                                      height: 84,
-                                                      width: 84,
-                                                      decoration: BoxDecoration(
-                                                        shape: BoxShape.circle,
-                                                        image: DecorationImage(
-                                                          image: AssetImage(
-                                                              informationPhotos[
-                                                                  i]),
-                                                        ),
-                                                      ),
-                                                    ),
-                                                    SizedBox(
-                                                      width: 16,
-                                                    ),
-                                                    Column(
-                                                      crossAxisAlignment:
-                                                          CrossAxisAlignment
-                                                              .start,
-                                                      children: [
-                                                        Text(
-                                                          informationTittle[i],
-                                                          style: TextStyle(
-                                                              fontSize: 18,
-                                                              fontWeight:
-                                                                  FontWeight
-                                                                      .w500,
-                                                              color: Color(
-                                                                  0xFFEFEFEF)),
-                                                        ),
-                                                        SizedBox(
-                                                          height: 6,
-                                                        ),
-                                                        Container(
-                                                          height: 63,
-                                                          width: 245,
-                                                          child: Text(
-                                                            informationDesc[i],
-                                                            style: TextStyle(
-                                                              fontSize: 14,
-                                                              color: Color(
-                                                                0xFFEFEFEF,
-                                                              ),
-                                                            ),
-                                                          ),
-                                                        )
-                                                      ],
-                                                    )
-                                                  ],
-                                                ),
-                                              )),
-                                        );
-                                      }),
-                                )),
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
+                    );
+                  }),
                 ),
               ),
             ],
