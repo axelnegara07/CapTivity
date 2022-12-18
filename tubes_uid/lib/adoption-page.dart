3bb291fc78a1f697bf37c5f2a468715bb66e5b06
Axel Shira Sapata Negara <axelshira88@gmail.com>
2022-12-18 23:29:41 +0700

    Update adoption-page.dart

--- tubes_uid/lib/adoption-page.dart
+++ tubes_uid/lib/adoption-page.dart
@@ -11,6 +11,16 @@ class AdoptionPage extends StatefulWidget {
 }
 
 class _AdoptionPageState extends State<AdoptionPage> {
+  List<String> adoptionPhotos = [
+    "assets/adoption/category/dog.png",
+    "assets/adoption/category/cat.png",
+    "assets/adoption/category/rabbit.png",
+    "assets/adoption/category/hamster.png",
+    "assets/adoption/category/bird.png"
+  ];
+
+  List<String> adoptionTittle = ["Dog", "Cat", "Rabbit", "Hamster", "Bird"];
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -42,6 +52,89 @@ class _AdoptionPageState extends State<AdoptionPage> {
                           color: Color(0xFFF1F1F1)),
                     ),
                   ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(20, 13, 0, 0),
+                child: Row(
+                  children: [
+                    Text(
+                      "Choose Category",
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Color(0xFF575757),
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 139,
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      color: Color(0xFFFF0000),
+                      size: 37,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(33, 0, 33, 0),
+                child: GridView.count(
+                  shrinkWrap: true,
+                  childAspectRatio: 0.96,
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 22,
+                  crossAxisSpacing: 30,
+                  children: List.generate(adoptionTittle.length, (i) {
+                    return InkWell(
+                      onTap: () {
+                        //berbeda dengan genre, language hanya bisa terpilih 1
+                        //konsep : ubah semua isSelected languages jadi false dan isi language yang terpilih jadi true
+                        // for(var j = 0; j < languages.length; j++){
+                        // languages[j].isSelected = false;
+                        // }
+                        // languages[i].isSelected = true; //ini diassign true dan tidak ada ! karena dicontoh flutix jika yg udah keselect dipencet maka tidak akan hilang
+                        // setState(() {});
+                      },
+                      child: Container(
+                        // padding: EdgeInsets.fromLTRB(42, 27, 42, 21),
+                        decoration: BoxDecoration(
+                          // image: DecorationImage(
+                          //     image: AssetImage(adoptionPhotos[i])),
+                          color: Color(0xFFBB9457),
+                          // color: languages[i].isSelected ? Color(0XFFFBD45F) : Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          // border: Border.all(color: languages[i].isSelected ? Color(0XFFFBD45F) :  Color(0XFFE7E7E7)),
+                        ),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 27,
+                            ),
+                            Container(
+                              height: 81,
+                              width: 81,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  image: AssetImage(adoptionPhotos[i]),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 4,
+                            ),
+                            Text(
+                              adoptionTittle[i],
+                              style: TextStyle(
+                                  color: Color(0XFFFFFFFF), fontSize: 22),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
                 ),
               ),
             ],
