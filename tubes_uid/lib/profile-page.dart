2d213c7e29b9b4016a99865ae9d8d82cb11ce51b
Axel Shira Sapata Negara <axelshira88@gmail.com>
2022-12-29 17:51:33 +0700

    Update profile-page.dart

--- tubes_uid/lib/profile-page.dart
+++ tubes_uid/lib/profile-page.dart
@@ -47,175 +47,182 @@ class _ProfilePageState extends State<ProfilePage> {
             backgroundColor: Color(0xFFBB9457),
             elevation: 0,
           ),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // SizedBox(
-              //   height: 10,
-              // ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                      child: Container(
-                        height: 135,
-                        width: 135,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
+          body: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Container(
+                          height: 135,
+                          width: 135,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                          ),
+                          child: Image.asset("assets/pp.png"),
                         ),
-                        child: Image.asset("assets/pp.png"),
                       ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Text(
-                      "Name:",
-                      style: TextStyle(fontSize: 16, color: Color(0xFF575757)),
-                    ),
-                    SizedBox(
-                      height: 3,
-                    ),
-                    Container(
-                      height: 35,
-                      child: TextFormField(
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        "Name:",
+                        style:
+                            TextStyle(fontSize: 16, color: Color(0xFF575757)),
+                      ),
+                      SizedBox(
+                        height: 3,
+                      ),
+                      Container(
+                        height: 35,
+                        child: TextFormField(
+                          validator: (value) {},
+                          cursorColor: Color(0xFFFFEFC7),
+                          style: TextStyle(fontSize: 14),
+                          decoration: InputDecoration(
+                              contentPadding: EdgeInsets.fromLTRB(11, 5, 4, 11),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide.none)),
+                          controller: name,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Phone:",
+                        style:
+                            TextStyle(fontSize: 16, color: Color(0xFF575757)),
+                      ),
+                      SizedBox(
+                        height: 3,
+                      ),
+                      Container(
+                        height: 35,
+                        child: TextFormField(
+                          validator: (value) {},
+                          cursorColor: Color(0xFFFFEFC7),
+                          style: TextStyle(fontSize: 14),
+                          decoration: InputDecoration(
+                              contentPadding: EdgeInsets.fromLTRB(11, 5, 4, 11),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide.none)),
+                          controller: phone,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Email:",
+                        style:
+                            TextStyle(fontSize: 16, color: Color(0xFF575757)),
+                      ),
+                      SizedBox(
+                        height: 3,
+                      ),
+                      Container(
+                        height: 35,
+                        child: TextFormField(
+                          validator: (value) {},
+                          cursorColor: Color(0xFFFFEFC7),
+                          style: TextStyle(fontSize: 14),
+                          decoration: InputDecoration(
+                              contentPadding: EdgeInsets.fromLTRB(11, 5, 4, 11),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide.none)),
+                          controller: email,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Region:",
+                        style:
+                            TextStyle(fontSize: 16, color: Color(0xFF575757)),
+                      ),
+                      SizedBox(
+                        height: 3,
+                      ),
+                      Container(
+                        height: 35,
+                        child: TextFormField(
+                          validator: (value) {},
+                          cursorColor: Color(0xFFFFEFC7),
+                          style: TextStyle(fontSize: 14),
+                          decoration: InputDecoration(
+                              contentPadding: EdgeInsets.fromLTRB(11, 5, 4, 11),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide.none)),
+                          controller: region,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Favorite Animal:",
+                        style:
+                            TextStyle(fontSize: 16, color: Color(0xFF575757)),
+                      ),
+                      SizedBox(
+                        height: 3,
+                      ),
+                      Container(
+                        height: 35,
+                        child: TextFormField(
+                          validator: (value) {},
+                          cursorColor: Color(0xFFFFEFC7),
+                          style: TextStyle(fontSize: 14),
+                          decoration: InputDecoration(
+                              contentPadding: EdgeInsets.fromLTRB(11, 5, 4, 11),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide.none)),
+                          controller: favAnimal,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Bio:",
+                        style:
+                            TextStyle(fontSize: 16, color: Color(0xFF575757)),
+                      ),
+                      SizedBox(
+                        height: 3,
+                      ),
+                      TextFormField(
+                        maxLines: 4,
+                        keyboardType: TextInputType.multiline,
                         validator: (value) {},
                         cursorColor: Color(0xFFFFEFC7),
                         style: TextStyle(fontSize: 14),
                         decoration: InputDecoration(
                             contentPadding: EdgeInsets.fromLTRB(11, 5, 4, 11),
-                            filled: true,
-                            fillColor: Colors.white,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide.none)),
-                        controller: name,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      "Phone:",
-                      style: TextStyle(fontSize: 16, color: Color(0xFF575757)),
-                    ),
-                    SizedBox(
-                      height: 3,
-                    ),
-                    Container(
-                      height: 35,
-                      child: TextFormField(
-                        validator: (value) {},
-                        cursorColor: Color(0xFFFFEFC7),
-                        style: TextStyle(fontSize: 14),
-                        decoration: InputDecoration(
-                            contentPadding: EdgeInsets.fromLTRB(11, 5, 4, 11),
-                            filled: true,
-                            fillColor: Colors.white,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide.none)),
-                        controller: phone,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      "Email:",
-                      style: TextStyle(fontSize: 16, color: Color(0xFF575757)),
-                    ),
-                    SizedBox(
-                      height: 3,
-                    ),
-                    Container(
-                      height: 35,
-                      child: TextFormField(
-                        validator: (value) {},
-                        cursorColor: Color(0xFFFFEFC7),
-                        style: TextStyle(fontSize: 14),
-                        decoration: InputDecoration(
-                            contentPadding: EdgeInsets.fromLTRB(11, 5, 4, 11),
-                            filled: true,
-                            fillColor: Colors.white,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide.none)),
-                        controller: email,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      "Region:",
-                      style: TextStyle(fontSize: 16, color: Color(0xFF575757)),
-                    ),
-                    SizedBox(
-                      height: 3,
-                    ),
-                    Container(
-                      height: 35,
-                      child: TextFormField(
-                        validator: (value) {},
-                        cursorColor: Color(0xFFFFEFC7),
-                        style: TextStyle(fontSize: 14),
-                        decoration: InputDecoration(
-                            contentPadding: EdgeInsets.fromLTRB(11, 5, 4, 11),
-                            filled: true,
-                            fillColor: Colors.white,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide.none)),
-                        controller: region,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      "Favorite Animal:",
-                      style: TextStyle(fontSize: 16, color: Color(0xFF575757)),
-                    ),
-                    SizedBox(
-                      height: 3,
-                    ),
-                    Container(
-                      height: 35,
-                      child: TextFormField(
-                        validator: (value) {},
-                        cursorColor: Color(0xFFFFEFC7),
-                        style: TextStyle(fontSize: 14),
-                        decoration: InputDecoration(
-                            contentPadding: EdgeInsets.fromLTRB(11, 5, 4, 11),
-                            filled: true,
-                            fillColor: Colors.white,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide.none)),
-                        controller: favAnimal,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      "Bio:",
-                      style: TextStyle(fontSize: 16, color: Color(0xFF575757)),
-                    ),
-                    SizedBox(
-                      height: 3,
-                    ),
-                    Container(
-                      height: 90,
-                      child: TextFormField(
-                        validator: (value) {},
-                        cursorColor: Color(0xFFFFEFC7),
-                        style: TextStyle(fontSize: 14),
-                        decoration: InputDecoration(
-                            // contentPadding: EdgeInsets.fromLTRB(11, 5, 4, 11),
                             filled: true,
                             fillColor: Colors.white,
                             border: OutlineInputBorder(
@@ -223,42 +230,45 @@ class _ProfilePageState extends State<ProfilePage> {
                                 borderSide: BorderSide.none)),
                         controller: bio,
                       ),
-                    ),
-                    Center(
-                      child: Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          child: Container(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 44, vertical: 10),
-                            decoration: BoxDecoration(
-                                color: Color(0xFFF99582A),
-                                borderRadius: BorderRadius.circular(10)),
-                            child: Text(
-                              "Save",
-                              style:
-                                  TextStyle(fontSize: 18, color: Colors.white),
-                            ),
-                          ),
-                          // onTap: () {
-                          //   // if (formKey.currentState!.validate()) {
-                          //   //   formKey.currentState!.save();
-                          //   //   Navigator.push(
-                          //   //       context,
-                          //   //       MaterialPageRoute(
-                          //   //           builder: (context) =>
-                          //   //               NavbarPage()));
-                          //   }
-                          //   // print(email.text);
-                          //   // print(password.text);
-                          // },
-                        ),
+                      SizedBox(
+                        height: 50,
                       ),
-                    )
-                  ],
+                      Center(
+                        child: Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 44, vertical: 10),
+                              decoration: BoxDecoration(
+                                  color: Color(0xFFF99582A),
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Text(
+                                "Save",
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.white),
+                              ),
+                            ),
+                            // onTap: () {
+                            //   // if (formKey.currentState!.validate()) {
+                            //   //   formKey.currentState!.save();
+                            //   //   Navigator.push(
+                            //   //       context,
+                            //   //       MaterialPageRoute(
+                            //   //           builder: (context) =>
+                            //   //               NavbarPage()));
+                            //   }
+                            //   // print(email.text);
+                            //   // print(password.text);
+                            // },
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ],
