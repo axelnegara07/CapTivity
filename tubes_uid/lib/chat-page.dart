f000266e1b55201cecfe0387fd5b035d5d22f595
Axel Shira Sapata Negara <axelshira88@gmail.com>
2023-01-04 22:11:19 +0800

    Update chat-page.dart

--- tubes_uid/lib/chat-page.dart
+++ tubes_uid/lib/chat-page.dart
@@ -42,181 +42,162 @@ class _ChatPageState extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        // Image.asset(
-        //   "assets/doodle.png",
-        //   height: MediaQuery.of(context).size.height,
-        //   width: MediaQuery.of(context).size.width,
-        //   fit: BoxFit.cover,
-        // ),
-        Scaffold(
-          resizeToAvoidBottomInset: false,
-          backgroundColor: Color(0xFFFFEFC7),
-          appBar: AppBar(
-            toolbarHeight: 70,
-            titleSpacing: 20,
-            title: Text(
-              "Chat",
-              style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w600,
-                  color: Color(0xFFF1F1F1)),
-            ),
-            automaticallyImplyLeading: false,
-            backgroundColor: Color(0xFFBB9457),
-            elevation: 0,
-          ),
-          body: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(
-                    "assets/doodle.png",
-                  ),
-                  fit: BoxFit.cover),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.fromLTRB(18, 17, 18, 0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color(0xFFFFEFC7),
+      appBar: AppBar(
+        toolbarHeight: 70,
+        titleSpacing: 20,
+        title: Text(
+          "Chat",
+          style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w600,
+              color: Color(0xFFF1F1F1)),
+        ),
+        automaticallyImplyLeading: false,
+        backgroundColor: Color(0xFFBB9457),
+        elevation: 0,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
+                "assets/doodle.png",
+              ),
+              fit: BoxFit.cover),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.fromLTRB(18, 17, 18, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
                     children: [
-                      Column(
-                        children: [
-                          Container(
-                            height: MediaQuery.of(context).size.height - 185,
-                            width: 380,
-                            child: Expanded(
-                              child: Container(
-                                  height: 122,
-                                  width: 380,
-                                  child: MediaQuery.removePadding(
-                                    context: context,
-                                    removeTop: true,
-                                    removeBottom: true,
-                                    child: ListView.builder(
-                                        shrinkWrap: true,
-                                        scrollDirection: Axis.vertical,
-                                        itemCount: chatPhotos.length,
-                                        itemBuilder: (context, i) {
-                                          return Padding(
-                                            padding: EdgeInsets.only(bottom: 8),
-                                            child: InkWell(
-                                              onTap: () {
-                                                Navigator.push(
-                                                    context,
-                                                    MaterialPageRoute(
-                                                        builder: (context) =>
-                                                            ChatDetailPage()));
-                                              },
-                                              child: Container(
-                                                  height: 82,
-                                                  width: 392,
-                                                  decoration: BoxDecoration(
-                                                    color: Color(0xFFBB9457),
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            10),
-                                                  ),
-                                                  child: Padding(
-                                                    padding:
-                                                        EdgeInsets.fromLTRB(
-                                                            13, 9, 0, 9),
-                                                    child: Row(
+                      Container(
+                        height: MediaQuery.of(context).size.height - 185,
+                        width: 380,
+                        child: Expanded(
+                          child: Container(
+                              height: 122,
+                              width: 380,
+                              child: MediaQuery.removePadding(
+                                context: context,
+                                removeTop: true,
+                                removeBottom: true,
+                                child: ListView.builder(
+                                    shrinkWrap: true,
+                                    scrollDirection: Axis.vertical,
+                                    itemCount: chatPhotos.length,
+                                    itemBuilder: (context, i) {
+                                      return Padding(
+                                        padding: EdgeInsets.only(bottom: 8),
+                                        child: InkWell(
+                                          onTap: () {
+                                            Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        ChatDetailPage()));
+                                          },
+                                          child: Container(
+                                              height: 82,
+                                              width: 392,
+                                              decoration: BoxDecoration(
+                                                color: Color(0xFFBB9457),
+                                                borderRadius:
+                                                    BorderRadius.circular(10),
+                                              ),
+                                              child: Padding(
+                                                padding: EdgeInsets.fromLTRB(
+                                                    13, 9, 0, 9),
+                                                child: Row(
+                                                  children: [
+                                                    Container(
+                                                      height: 64,
+                                                      width: 64,
+                                                      decoration: BoxDecoration(
+                                                        shape: BoxShape.circle,
+                                                        image: DecorationImage(
+                                                          image: AssetImage(
+                                                              chatPhotos[i]),
+                                                        ),
+                                                      ),
+                                                    ),
+                                                    SizedBox(
+                                                      width: 15,
+                                                    ),
+                                                    Column(
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .start,
                                                       children: [
-                                                        Container(
-                                                          height: 64,
-                                                          width: 64,
-                                                          decoration:
-                                                              BoxDecoration(
-                                                            shape:
-                                                                BoxShape.circle,
-                                                            image:
-                                                                DecorationImage(
-                                                              image: AssetImage(
-                                                                  chatPhotos[
-                                                                      i]),
+                                                        Row(
+                                                          children: [
+                                                            Container(
+                                                              width: 225,
+                                                              child: Text(
+                                                                chatTittle[i],
+                                                                style: TextStyle(
+                                                                    fontSize:
+                                                                        18,
+                                                                    fontWeight:
+                                                                        FontWeight
+                                                                            .w500,
+                                                                    color: Color(
+                                                                        0xFFEFEFEF)),
+                                                              ),
                                                             ),
-                                                          ),
+                                                            Text(
+                                                              chatDate[i],
+                                                              style: TextStyle(
+                                                                  fontSize: 14,
+                                                                  fontWeight:
+                                                                      FontWeight
+                                                                          .w400,
+                                                                  color: Color(
+                                                                      0xFFEFEFEF)),
+                                                            ),
+                                                          ],
                                                         ),
                                                         SizedBox(
-                                                          width: 15,
+                                                          height: 10,
                                                         ),
-                                                        Column(
-                                                          crossAxisAlignment:
-                                                              CrossAxisAlignment
-                                                                  .start,
-                                                          children: [
-                                                            Row(
-                                                              children: [
-                                                                Container(
-                                                                  width: 225,
-                                                                  child: Text(
-                                                                    chatTittle[
-                                                                        i],
-                                                                    style: TextStyle(
-                                                                        fontSize:
-                                                                            18,
-                                                                        fontWeight:
-                                                                            FontWeight
-                                                                                .w500,
-                                                                        color: Color(
-                                                                            0xFFEFEFEF)),
-                                                                  ),
-                                                                ),
-                                                                Text(
-                                                                  chatDate[i],
-                                                                  style: TextStyle(
-                                                                      fontSize:
-                                                                          14,
-                                                                      fontWeight:
-                                                                          FontWeight
-                                                                              .w400,
-                                                                      color: Color(
-                                                                          0xFFEFEFEF)),
-                                                                ),
-                                                              ],
+                                                        Container(
+                                                          height: 21,
+                                                          width: 245,
+                                                          child: Text(
+                                                            chatDesc[i],
+                                                            style: TextStyle(
+                                                              fontSize: 14,
+                                                              color: Color(
+                                                                  0xFFEFEFEF),
                                                             ),
-                                                            SizedBox(
-                                                              height: 10,
-                                                            ),
-                                                            Container(
-                                                              height: 21,
-                                                              width: 245,
-                                                              child: Text(
-                                                                chatDesc[i],
-                                                                style:
-                                                                    TextStyle(
-                                                                  fontSize: 14,
-                                                                  color: Color(
-                                                                      0xFFEFEFEF),
-                                                                ),
-                                                              ),
-                                                            )
-                                                          ],
+                                                          ),
                                                         )
                                                       ],
-                                                    ),
-                                                  )),
-                                            ),
-                                          );
-                                        }),
-                                  )),
-                            ),
-                          )
-                        ],
-                      ),
+                                                    )
+                                                  ],
+                                                ),
+                                              )),
+                                        ),
+                                      );
+                                    }),
+                              )),
+                        ),
+                      )
                     ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
